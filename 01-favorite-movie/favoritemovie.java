4d3368c99197277882ec35d2c1b2e2c56b99a410
ericc380 <114923803+ericc380@users.noreply.github.com>
2022-10-04 08:19:46 -0400

    Add files via upload

---
+++ 01-favorite-movie/favoritemovie.java
@@ -0,0 +1,24 @@
+
+/**
+ * i will describe my favorite movie in a few print statements here.
+ *
+ *
+ * @author (Eric Chen)
+ * @version (09/21/22)
+ */
+public class favoritemovie
+{
+    public static void main(String[] args) {
+    System.out.println("My favorite movie isn't exactly a movie I dont know but baically it's this summer themed series where 2 moms are best friends and their kids spend summer together at this beach house every year this year.");
+    
+    System.out.println("The daughter of one of the moms would someday marry the mother's bestfriend son ");
+    
+    System.out.println("Over this summer there's been a lot of love, breakups, new friendships. ");
+      
+      System.out.println("and finding out that their mom's best friend aka the other mom has cancer and her sons begged her to not give up after the others figured out they were devastated. ");
+    
+    System.out.println("the mom finally decided to give another chance for treatment.");
+    
+    System.out.println("Finally in the end the main character kisses her crush  ");
+}
+}
